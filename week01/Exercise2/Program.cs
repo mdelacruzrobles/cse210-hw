068b3543adc08c9c65100becee4bcc8148e0e594
mdelacruzrobles <mdelacruzrobles@byupathway.edu>
2026-03-10 21:55:29 -0400

    exercise02

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        string letter = "";
+        int numberConversion = int.Parse(grade);
+        if (numberConversion >= 90)
+        {
+            letter = "A";
+        }
+        else if (numberConversion >= 80 && numberConversion < 90)
+        {
+            letter = "B";
+        }
+        else if (numberConversion >= 70 && numberConversion < 80)
+        {
+            letter = "C";
+        }
+        else if (numberConversion >= 60 && numberConversion < 70)
+        {
+            letter = "D";
+        }
+        else if (numberConversion < 60)
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade letter is {letter}");
+
+        if (numberConversion >= 70)
+        {
+            Console.WriteLine("You have passed");
+        }
+        else
+        {
+            Console.WriteLine("Try harder next time");
+        }
     }
 }
