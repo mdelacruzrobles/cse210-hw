8c3db523d92babc29deebcccf07f4df8bd6551ce
mdelacruzrobles <mdelacruzrobles@byupathway.edu>
2026-03-14 15:33:41 -0400

    week01

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program");
+    }
+    static string PromptUserName()
+    {
+        string userName;
+        Console.Write("Please enter your name: ");
+        userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        int userNumber;
+        Console.Write("Please enter your favorite number: ");
+        userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+    static int SquareNumber(int number)
+    {
+        int square;
+        square = number * number;
+        return square;
+    }
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"Brother {name}, the square of your number is {square}");
+    
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string user = PromptUserName();
+        int number = PromptUserNumber();
+        int squareNumberUser = SquareNumber(number);
+        DisplayResult(user, squareNumberUser);
     }
 }
